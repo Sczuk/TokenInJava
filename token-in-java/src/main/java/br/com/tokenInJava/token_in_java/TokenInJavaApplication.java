8b0d6625f5254b2b8d686e173c797358891b09e1
Sczuk <hgreasczuk@gmail.com>
2026-03-17 17:22:05 -0300

    feat(main)

---
+++ token-in-java/src/main/java/br/com/tokenInJava/token_in_java/TokenInJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.tokenInJava.token_in_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenInJavaApplication.class, args);
+	}
+
+}
