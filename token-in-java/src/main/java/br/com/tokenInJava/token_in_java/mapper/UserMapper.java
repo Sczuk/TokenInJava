69118c68b5da0b36aaef5362e5ad6086fe7e9813
Sczuk <hgreasczuk@gmail.com>
2026-04-07 20:05:32 -0300

    fix(Mappers): management manually of the Mappers beans

--- token-in-java/src/main/java/br/com/tokenInJava/token_in_java/mapper/UserMapper.java
+++ token-in-java/src/main/java/br/com/tokenInJava/token_in_java/mapper/UserMapper.java
@@ -1,11 +1,11 @@
-package br.com.tokenInJava.token_in_java.mapper.user;
+package br.com.tokenInJava.token_in_java.mapper;
 
 import br.com.tokenInJava.token_in_java.DTO.request.user.UserRequest;
 import br.com.tokenInJava.token_in_java.DTO.response.user.UserResponse;
 import br.com.tokenInJava.token_in_java.entity.user.User;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface UserMapper {
 
     UserResponse toDTO(User user);
--- token-in-java/src/main/java/br/com/tokenInJava/token_in_java/mapper/UserStatusMapper.java
+++ token-in-java/src/main/java/br/com/tokenInJava/token_in_java/mapper/UserStatusMapper.java
@@ -1,10 +1,10 @@
-package br.com.tokenInJava.token_in_java.mapper.user;
+package br.com.tokenInJava.token_in_java.mapper;
 
 import br.com.tokenInJava.token_in_java.DTO.response.user.UserStatusResponse;
 import br.com.tokenInJava.token_in_java.entity.user.User;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface UserStatusMapper {
 
     UserStatusResponse toDTO(User user);
