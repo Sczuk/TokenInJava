2beb7cc58ecc8c1097b192d2b9505138fa48260a
Sczuk <hgreasczuk@gmail.com>
2026-04-08 18:06:53 -0300

    feat(authentication)

---
+++ token-in-java/src/main/java/br/com/tokenInJava/token_in_java/useCases/authenticationUseCases/AuthenticationUseCase.java
@@ -0,0 +1,20 @@
+package br.com.tokenInJava.token_in_java.useCases.authenticationUseCases;
+
+import br.com.tokenInJava.token_in_java.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationUseCase implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email);
+    }
+}
