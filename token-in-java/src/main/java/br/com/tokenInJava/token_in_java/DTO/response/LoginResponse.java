f192460a467515c90ee821ecdfa27f809434d7bb
Sczuk <hgreasczuk@gmail.com>
2026-04-08 18:08:19 -0300

    feat(Response): response of login

---
+++ token-in-java/src/main/java/br/com/tokenInJava/token_in_java/DTO/response/LoginResponse.java
@@ -0,0 +1,12 @@
+package br.com.tokenInJava.token_in_java.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class LoginResponse {
+
+    private String token;
+
+}
